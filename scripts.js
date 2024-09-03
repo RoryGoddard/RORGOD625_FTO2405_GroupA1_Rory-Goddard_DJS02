d7c40a7efec7a10d7b38f69998e316e61bbc5e14
Rory Goddard <goddardrory@gmail.com>
2024-09-03 19:47:29 +0200

    Fix: isEmpty function checks if fields are empty

--- scripts.js
+++ scripts.js
@@ -5,5 +5,26 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = Math.floor(dividend / divider);
+  if (isEmpty(dividend) || isEmpty(divider)) {
+    alert(
+      "Division not performed. Both values are required in inputs. Try again"
+    );
+  } else if (!isNumeric(dividend) || !isNumeric(divider)) {
+    alert("Please input valid whole numbers");
+  } else {
+    result.innerText = Math.floor(dividend / divider);
+  }
 });
+
+function isNumeric(num) {
+  return (
+    (typeof num === "number" ||
+      (typeof num === "string" && num.trim() !== "")) &&
+    !isNaN(num)
+  );
+}
+
+function isEmpty(num) {
+  console.log(num)
+  return num === "";
+}
