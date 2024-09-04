8d730ad4e53bea37ed9eb84cca79365330181825
Rory Goddard <goddardrory@gmail.com>
2024-09-04 09:21:25 +0200

    Fix: Change alerts to result.innerText

--- scripts.js
+++ scripts.js
@@ -6,18 +6,16 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   if (isEmpty(dividend) || isEmpty(divider)) {
-    alert(
-      "Division not performed. Both values are required in inputs. Try again"
-    );
+    result.innerText = "Division not performed. Both values are required in inputs. Try again";
   } else if (dividend === "YOLO" && divider === "+++") {
     document.write("Something critical went wrong. Please reload the page")
     console.trace();
   } else if (!isNumeric(dividend) || !isNumeric(divider)) {
-    alert("Please input valid whole numbers");
+    result.innerText = "Please input valid whole numbers";
   } else if (dividend === "0" || divider === "0") {
     result.innerText = ""
     console.trace();
-    alert("Division not performed. Invalid number provided. Try again");
+    result.innerText = "Division not performed. Invalid number provided. Try again";
   } else {
     result.innerText = Math.floor(dividend / divider);
   }
